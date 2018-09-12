84a191619491ecec08d06dce63ef86e3f95bdcaa
Paul Roth <paulroth3d@gmail.com>
2018-09-12 13:47:40 -0500

    UPDATE: support config file

---
+++ config/default.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    //-- base defults
+    "DEFAULT": {
+        //-- default port
+        "PORT": 5000
+    },
+
+    //-- base url redirection
+    "BASE_REDIRECT": "https://www.google.com"
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
+const config = require('config');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || config.DEFAULT.PORT;
+
+/**
+ * base page redirect
+ */
+function handleBaseRedirect(req, resp) {
+  const redirectURL = (process.env.BASE_REDIRECT || config.BASE_REDIRECT);
+  resp.redirect(redirectURL);
+}
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', handleBaseRedirect) //(req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
