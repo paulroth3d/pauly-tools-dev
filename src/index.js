053fc5bbe503b11074ed8dee9da54dbe6bf185d7
Paul Roth <paulroth3d@gmail.com>
2018-09-12 22:49:12 -0500

    UPDATE: move the example ejs to /example

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function handleBaseRedirect(req, resp) {
 /**
  * Redirect for the title page
  */
-function handleTitlePageRequest(req, resp){
+function handleTitlePageRequest(req, resp) {
   resp.render('pages/index');
 }
 
@@ -24,6 +24,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', handleBaseRedirect) //(req, res) => res.render('pages/index'))
+  .get('/', handleBaseRedirect)
+  .get('/example', (req, res) => res.render('pages/index'))
   .get('/titlePage', handleTitlePageRequest)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
