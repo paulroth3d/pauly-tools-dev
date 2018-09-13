e34d9e46e4104c1ee5e848d2f7bf1279dafa0aa0
Paul Roth <paulroth3d@gmail.com>
2018-09-12 22:47:38 -0500

    UPDTE: support base redirect

--- config/default.js
+++ config/default.js
@@ -7,5 +7,11 @@ module.exports = {
     },
 
     //-- base url redirection
-    "BASE_REDIRECT": "https://www.google.com"
+    "BASE_REDIRECT": "https://www.google.com",
+    
+	//-- https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+	"statusCodes": {
+		"unauthorized": 401,
+		"unauthorizedText": "Unauthorized"
+	}
 }
--- src/index.js
+++ src/index.js
@@ -12,10 +12,18 @@ function handleBaseRedirect(req, resp) {
   resp.redirect(redirectURL);
 }
 
+/**
+ * Redirect for the title page
+ */
+function handleTitlePageRequest(req, resp){
+  resp.render('pages/index');
+}
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', handleBaseRedirect) //(req, res) => res.render('pages/index'))
+  .get('/titlePage', handleTitlePageRequest)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
