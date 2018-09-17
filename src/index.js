abd627881296e4e545c7a0588469c36f297802c8
Paul Roth <paulroth3d@gmail.com>
2018-09-17 11:26:20 -0500

    INTERNAL: add in investigations

--- src/index.js
+++ src/index.js
@@ -19,6 +19,17 @@ function handleTitlePageRequest(req, resp) {
   resp.render('pages/index');
 }
 
+//-- investigate further
+//-- react / node / express / webpack
+//-- https://hackernoon.com/full-stack-web-application-using-react-node-js-express-and-webpack-97dbd5b9d708
+//-- https://github.com/crsandeep/simple-react-full-stack
+//-- react app from scratch - webpack 4
+//-- https://medium.freecodecamp.org/part-1-react-app-from-scratch-using-webpack-4-562b1d231e75
+//-- webpack
+//-- https://github.com/crsandeep/simple-react-full-stack (one for each page)
+
+//-- http://ccoenraets.github.io/es6-tutorial-data/babel-webpack/
+//-- http://ccoenraets.github.io/es6-tutorial/
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
