8ed4d59f9567216d1aa96c079df3a5c91ad643c9
Paul Roth <paulroth3d@gmail.com>
2018-11-05 13:36:06 -0600

    UPDATE: support conditionals within the element

--- src/siteSrc/script/app/reactCourse.js
+++ src/siteSrc/script/app/reactCourse.js
@@ -9,10 +9,38 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: 'Paul',
-      type: 'good'
+      people: [{
+        name: 'Paul',
+        type: 'good',
+        message: 'At least I try to be'
+      }, {
+        name: 'John',
+        type: 'bad',
+        message: null
+      }],
+      currentPerson: null
     };
-    this.switchNameHandler = this.switchNameHandler.bind(this);
+    this.state.currentPerson = this.state.people[0];
+    // this.switchNameHandler = this.switchNameHandler.bind(this);
+  }
+
+  switchNameHandler = () => {
+    const newState = this.state;
+
+    var currentPersonIndex = 0;
+    if (this.state.currentPerson.name === 'John') {
+      currentPersonIndex = 1;
+    }
+
+    var newPersonIndex = 0;
+    if (currentPersonIndex === 0) {
+      newPersonIndex = 1;
+    } else {
+      newPersonIndex = 0;
+    }
+
+    newState.currentPerson = this.state.people[newPersonIndex];
+    this.setState(newState);
   }
 
   /*
@@ -26,9 +54,11 @@ class App extends Component {
   }
   */
 
+  /*
   switchNameHandler() {
     console.log('switch name was called');
   }
+  */
 
   render() {
 
@@ -36,7 +66,7 @@ class App extends Component {
       <div className='App'>
         <h1>Hi. I'm a react app</h1>
         <p>This works</p>
-        <Person name={this.state.name} type={this.state.type}>At least I try to be</Person>
+        <Person name={this.state.currentPerson.name} type={this.state.currentPerson.type}>{this.state.currentPerson.message}</Person>
         <button onClick={this.switchNameHandler}>Switch Name</button>
       </div>
     )
--- src/siteSrc/script/components/Person.js
+++ src/siteSrc/script/components/Person.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Person = (props) => {
+  var personMessage = null;
+  if (props.children && props.children.length > 0) {
+    personMessage = <b>{props.children}</b>;
+  }
   return (
-    <p>Hello, my name is {props.name}. I am a {props.type} person. {props.children}</p>
+    <p>Hello, my name is {props.name}. I am a {props.type} person. {personMessage}</p>
   );
 }
 
